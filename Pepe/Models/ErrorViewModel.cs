0c9d4b16c5eae6ef4b27fac88c494378e5cf03a6
FherHR8 <f.herrero@salesianospizarrales.educamos.com>
2023-12-14 20:42:52 +0100

    Proyecto de interfaces
    Final

---
+++ Pepe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pepe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
